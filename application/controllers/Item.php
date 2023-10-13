f4cc24b62968fa392ab6a179bc91a3b7320a1fe4
mangedod <aqouzzz@gmail.com>
2023-10-13 15:13:12 +0700

    tambah pecahan sku

---
+++ application/controllers/Item.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+class Item extends RestController
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Mrest', 'row');
+		$this->methods['index_get']['limit'] = 2000;
+	}
+	public function index_get()
+	{
+		// $data = $this->db->get('telah_masuk')->result();
+		// $this->response($data, RestController::HTTP_OK);
+
+		$id = $this->get('id');
+		if ($id === null) {
+			$masuk = $this->row->getItem();
+		} else {
+			$masuk = $this->row->getItem($id);
+		}
+		if ($masuk) {
+			$this->response([
+				'status' => true,
+				'data' => $masuk
+			], RESTController::HTTP_OK);
+		} else {
+			$this->response([
+				'status' => false,
+				'pesan' => "Data tidak ditemukan"
+			], RESTController::HTTP_NOT_FOUND);
+		}
+	}
+}
--- application/models/Mrest.php
+++ application/models/Mrest.php
@@ -3,25 +3,57 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Mrest extends CI_Model
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->database(); // Inisialisasi objek database
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->database(); // Inisialisasi objek database
+  }
+  public function getMasuk($id = null)
+  {
+    if ($id === null) {
+      return $this->db->get('telah_masuk')->result_array();
+    } else {
+      return $this->db->get_where('telah_masuk', ['id_telah_masuk' => $id])->result_array();
     }
-    public function getMasuk($id = null)
-    {
-        if ($id === null) {
-            return $this->db->get('telah_masuk')->result_array();
-        } else {
-            return $this->db->get_where('telah_masuk', ['id_telah_masuk' => $id])->result_array();
-        }
+  }
+  public function getDataBarang($id = null)
+  {
+    if ($id === null) {
+      return $this->db->get('migrasi_data')->result_array();
+    } else {
+      return $this->db->get_where('migrasi_data', ['id_migrasi' => $id])->result_array();
     }
-    public function getDataBarang($id = null)
-    {
-        if ($id === null) {
-            return $this->db->get('migrasi_data')->result_array();
-        } else {
-            return $this->db->get_where('migrasi_data', ['id_migrasi' => $id])->result_array();
-        }
+  }
+  public function getKategori($id = null)
+  {
+    if ($id === null) {
+      return $this->db->get('inv_kategori')->result_array();
+    } else {
+      return $this->db->get_where('inv_kategori', ['id_inv_kate' => $id])->result_array();
     }
+  }
+  public function getItem($id = null)
+  {
+    if ($id === null) {
+      return $this->db->get('inv_item')->result_array();
+    } else {
+      return $this->db->get_where('inv_item', ['id_inv_item' => $id])->result_array();
+    }
+  }
+  public function getFinishing($id = null)
+  {
+    if ($id === null) {
+      return $this->db->get('inv_finishing')->result_array();
+    } else {
+      return $this->db->get_where('inv_finishing', ['id_inv_finishing' => $id])->result_array();
+    }
+  }
+  public function getWarna($id = null)
+  {
+    if ($id === null) {
+      return $this->db->get('inv_warna')->result_array();
+    } else {
+      return $this->db->get_where('inv_warna', ['id_inv_warna' => $id])->result_array();
+    }
+  }
 }
